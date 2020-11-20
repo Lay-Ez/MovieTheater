0c78102ff913d89acf1fe9999398046e9aa6f65e
Romano_M <deep.depp@yandex.com>
2020-11-20 14:44:49 +0300

    Use sealed class for viewstate

--- app/src/main/java/com/example/movietheater/ui/movieslistscreen/ui/viewmodel/Contract.kt
+++ app/src/main/java/com/example/movietheater/ui/movieslistscreen/ui/viewmodel/Contract.kt
@@ -1,14 +1,21 @@
 package com.example.movietheater.ui.movieslistscreen.ui.viewmodel
 
 import com.example.movietheater.base.viewmodel.Event
-import com.example.movietheater.base.viewmodel.Status
 import com.example.movietheater.ui.data.model.UiMovieModel
 
-data class MoviesViewState(
-    val status: Status,
-    val movieList: List<UiMovieModel>,
-    val error: Throwable?
-)
+sealed class MoviesViewState {
+
+    data class Content(
+        val movieList: List<UiMovieModel>
+    ) : MoviesViewState()
+
+    data class Error(
+        val error: Throwable
+    ) : MoviesViewState()
+
+    object Loading : MoviesViewState()
+}
+
 
 sealed class DataEvent : Event {
     data class OnMoviesLoaded(val movieList: List<UiMovieModel>) : DataEvent()
@@ -18,4 +25,5 @@ sealed class DataEvent : Event {
 
 sealed class UiEvent : Event {
     object LoadMovies : UiEvent()
+    object ForceLoadMovies : UiEvent()
 }
